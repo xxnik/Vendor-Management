5a29f77caeb160c2da60e3d23044f0fd3f4fb631
Delta Student <223nikhilanand@gmail.com>
2026-07-09 08:53:55 +0530

    modifying url

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
