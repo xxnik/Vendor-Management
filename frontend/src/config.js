d6fd73a0d9e0442b4fce235003e56bf9fb2af5d3
Delta Student <223nikhilanand@gmail.com>
2026-07-10 11:12:54 +0530

    ...

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://vendor-management-xd2c.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
